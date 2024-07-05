6b17f970d72a7c895441464b096378870e97c4a9
hoo <tbnsok40@gmail.com>
2024-07-06 00:37:52 +0900

    feat: define Corp as enum

---
+++ common/enum/company.go
@@ -0,0 +1,11 @@
+package compnay
+
+type Company string
+
+const (
+	Kakaopay   Company = "Kakaopay"
+	Oliveyoung Company = "Oliveyoung"
+	Banksalad  Company = "Banksalad"
+	Toss       Company = "Toss"
+	Daangn     Company = "Daangn"
+)
