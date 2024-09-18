473a4580006d840b440fa93e62b733e5661ee363
hoo <tbnsok40@gmail.com>
2024-09-18 18:32:10 +0900

    feat: apply gin framework

--- go.mod
+++ go.mod
@@ -1,32 +1,52 @@
 module blog-gopher
 
-go 1.21
+go 1.21.0
+
+toolchain go1.21.1
 
 require (
-	github.com/BurntSushi/toml v1.4.0 // indirect
-	github.com/PuerkitoBio/goquery v1.9.2 // indirect
-	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/PuerkitoBio/goquery v1.9.2
+	github.com/gin-gonic/gin v1.10.0
+	github.com/naoina/toml v0.1.1
+	go.mongodb.org/mongo-driver v1.17.0
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
-	github.com/gofiber/fiber/v2 v2.52.5 // indirect
-	github.com/google/uuid v1.5.0 // indirect
-	github.com/klauspost/compress v1.17.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/bytedance/sonic v1.12.2 // indirect
+	github.com/bytedance/sonic/loader v0.2.0 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.22.1 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/naoina/go-stringutil v0.1.0 // indirect
-	github.com/naoina/toml v0.1.1 // indirect
-	github.com/richardlehane/mscfb v1.0.4 // indirect
-	github.com/richardlehane/msoleps v1.0.3 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
-	github.com/xuri/excelize/v2 v2.8.1 // indirect
-	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	golang.org/x/arch v0.10.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,189 +1,84 @@
 package main
 
 import (
-	. "blog-gopher/common/types"
-	"blog-gopher/scrapper/banksalad"
-	"blog-gopher/scrapper/daangn"
-	"blog-gopher/scrapper/kakaopay"
-	"blog-gopher/scrapper/oliveyoung"
-	"blog-gopher/scrapper/toss"
+	"blog-gopher/common/dto"
+	"blog-gopher/config"
+	"blog-gopher/repository"
+	"blog-gopher/service"
+	"flag"
+	"github.com/gin-gonic/gin"
 	"log"
-	"sync"
-	"time"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
-//func main() {
-//	// 엑셀 파일 생성
-//	f := excelize.NewFile()
-//
-//	// 시트 이름 지정
-//	sheetName := "PerformanceResults"
-//	f.NewSheet(sheetName)
-//
-//	// 헤더 작성 (Iteration, Channel, Goroutine, Synchronous, Select)
-//	headers := []string{"Iteration", "Channel", "Goroutine", "Synchronous", "Select"}
-//	for i, header := range headers {
-//		cell, _ := excelize.CoordinatesToCellName(i+1, 1) // A1, B1, C1, D1, E1
-//		f.SetCellValue(sheetName, cell, header)
-//	}
-//
-//	// 10회 반복 실행
-//	for iteration := 1; iteration <= 10; iteration++ {
-//		// Iteration 값 설정
-//		cell, _ := excelize.CoordinatesToCellName(1, iteration+1) // A2, A3, ...
-//		f.SetCellValue(sheetName, cell, iteration)
-//
-//		// Channel 방식
-//		//start := time.Now()
-//		//useChannel() // 실제 Channel 방식 함수 호출
-//		//elapsed := time.Since(start)
-//		//cell, _ = excelize.CoordinatesToCellName(2, iteration+1) // B2, B3, ...
-//		//f.SetCellValue(sheetName, cell, fmt.Sprintf("%.3f", elapsed.Seconds()))
-//
-//		// Goroutine 방식
-//		goroutineStart := time.Now()
-//		callGoroutine() // 실제 Goroutine 방식 함수 호출
-//		goroutineStartElapsed := time.Since(goroutineStart)
-//		cell, _ = excelize.CoordinatesToCellName(3, iteration+1) // C2, C3, ...
-//		f.SetCellValue(sheetName, cell, fmt.Sprintf("%.3f", goroutineStartElapsed.Seconds()))
-//
-//		// Synchronous 방식
-//		syncStart := time.Now()
-//		callSynchronous() // 실제 Synchronous 방식 함수 호출
-//		syncElapsed := time.Since(syncStart)
-//		cell, _ = excelize.CoordinatesToCellName(4, iteration+1) // D2, D3, ...
-//		f.SetCellValue(sheetName, cell, fmt.Sprintf("%.3f", syncElapsed.Seconds()))
-//
-//		// Select 방식
-//		selectStart := time.Now()
-//		useSelect() // 실제 Select 방식 함수 호출
-//		selectElapsed := time.Since(selectStart)
-//		cell, _ = excelize.CoordinatesToCellName(5, iteration+1) // E2, E3, ...
-//		f.SetCellValue(sheetName, cell, fmt.Sprintf("%.3f", selectElapsed.Seconds()))
-//	}
-//
-//	// 엑셀 파일 저장
-//	if err := f.SaveAs("performance_results.xlsx"); err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	log.Println("엑셀 파일에 성능 결과가 저장되었습니다.")
-//}
+var pathFlag = flag.String("config", "./config.toml", "config set up")
 
 func main() {
-	start := time.Now()
-	var result []Post
-	result = callGoroutineChannel(result)
-	log.Println("Total :", len(result))
-	elapsed := time.Since(start)
-	log.Printf("[Goroutine w Channel] Execution Time: %s\n", elapsed)
+	flag.Parse()
+	c := config.NewConfig(*pathFlag)
+	var s *service.Service
+	if repository, err := repository.NewRepository(c); err != nil {
+		panic(err)
+	} else {
+		s = service.NewService(repository)
+	}
+
+	r := gin.Default()
+	r.GET("/posts", func(c *gin.Context) {
+		posts := getPosts(c, s)
+		c.JSON(http.StatusOK, gin.H{
+			"posts": posts,
+		})
+	})
+
+	r.POST("/", func(c *gin.Context) {
+		s.UpdateAllPosts()
+	})
+	r.Run()
 }
 
-func callGoroutineChannel(result []Post) []Post {
-	scrapers := []func() []Post{
-		kakaopay.CallApi,
-		oliveyoung.CallApi,
-		daangn.CallApi,
-		toss.CallApi,
-		banksalad.CallApi,
-	}
-	resultChan := make(chan []Post, len(scrapers))
-
-	var wg sync.WaitGroup
-	for _, scraper := range scrapers {
-		wg.Add(1)
-		go func(scrapeFunc func() []Post) {
-			defer wg.Done()
-			resultChan <- scrapeFunc()
-		}(scraper)
-	}
-
-	// 결과 수집을 위한 고루틴
-	go func() {
-		wg.Wait()
-		close(resultChan)
-	}()
-
-	for posts := range resultChan {
-		result = append(result, posts...)
-	}
-
-	return result
+type Category struct {
+	Category string `form:"category"`
 }
 
-func callGoroutine(result []Post) []Post {
-	var mu sync.Mutex
-	var wg sync.WaitGroup
+func getPosts(c *gin.Context, s *service.Service) []dto.PostDTO {
+	pageStr := c.Query("page")         // 쿼리 파라미터에서 페이지 번호를 가져옵니다.
+	pageSizeStr := c.Query("pageSize") // 쿼리 파라미터에서 페이지 크기를 가져옵니다.
 
-	// 회사별 스크래핑 함수들을 goroutine으로 실행
-	scrapers := []func() []Post{
-		kakaopay.CallApi,
-		oliveyoung.CallApi,
-		daangn.CallApi,
-		toss.CallApi,
-		banksalad.CallApi,
+	// 기본 값 설정
+	page := 1
+	pageSize := 20
+
+	// 쿼리 파라미터에서 페이지 번호와 페이지 크기를 정수로 변환
+	if pageStr != "" {
+		if p, err := strconv.Atoi(pageStr); err == nil {
+			page = p
+		}
+	}
+	if pageSizeStr != "" {
+		if ps, err := strconv.Atoi(pageSizeStr); err == nil {
+			pageSize = ps
+		}
 	}
 
-	for _, scraper := range scrapers {
-		wg.Add(1)
-		go func(scrapeFunc func() []Post) {
-			defer wg.Done()
-
-			// 스크래핑 결과 가져오기
-			posts := scrapeFunc()
-
-			// 결과를 안전하게 result에 추가하기 위해 mutex 사용
-			mu.Lock()
-			result = append(result, posts...)
-			mu.Unlock()
-		}(scraper)
+	var category Category
+	if err := c.ShouldBind(&category); err != nil {
+		log.Fatalln("Something wrong", err)
 	}
-	// 모든 goroutine이 완료될 때까지 대기
-	wg.Wait()
-	return result
-}
+	categoryStrings := category.Category
+	var splits []string
+	if categoryStrings != "" {
+		splits = strings.Split(categoryStrings, ",")
+	} else {
+		splits = []string{}
+	}
 
-//func callGoroutineChannelSelect(result []Post) []Post {
-//
-//	doneChan := make(chan struct{}) // 모든 작업이 끝났는지 체크하는 채널
-//	scrapers := []func() []Post{
-//		kakaopay.CallApi,
-//		oliveyoung.CallApi,
-//		daangn.CallApi,
-//		toss.CallApi,
-//		banksalad.CallApi,
-//	}
-//	resultChan := make(chan []Post, len(scrapers))
-//
-//	// 각 스크래핑 함수를 비동기적으로 실행
-//	for _, scraper := range scrapers {
-//		go func(scrapeFunc func() []Post) {
-//			resultChan <- scrapeFunc()
-//		}(scraper)
-//	}
-//
-//	// 별도의 goroutine에서 결과를 수집
-//	go func() {
-//		for i := 0; i < len(scrapers); i++ {
-//			select {
-//			case posts := <-resultChan:
-//				// 결과가 도착하면 비동기적으로 처리
-//				result = append(result, posts...)
-//			}
-//		}
-//		doneChan <- struct{}{} // 결과 수집 완료를 알림
-//	}()
-//
-//	// 모든 작업이 완료될 때까지 대기 (doneChan으로 비동기 완료 확인)
-//	<-doneChan
-//	return result
-//}
-
-func callSynchronous(result []Post) []Post {
-	result = append(result, kakaopay.CallApi()...)
-	result = append(result, oliveyoung.CallApi()...)
-	result = append(result, daangn.CallApi()...)
-	result = append(result, toss.CallApi()...)
-	result = append(result, banksalad.CallApi()...)
-	return result
+	results := s.GetPosts(splits, page, pageSize)
+	response := make([]dto.PostDTO, len(results))
+	for idx, result := range results {
+		response[idx] = dto.ConvertToDTO(result)
+	}
+	return response
 }
