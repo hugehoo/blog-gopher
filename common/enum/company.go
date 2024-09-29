0288d8d0c5f18ccfede7da0a831cde5c44ac18e8
hoo <tbnsok40@gmail.com>
2024-09-29 17:06:05 +0900

    feat: add new corp constants

--- common/enum/company.go
+++ common/enum/company.go
@@ -3,13 +3,17 @@ package compnay
 type Company string
 
 const (
-	KAKAOPAY  Company = "KAKAOPAY"
-	OLIVE     Company = "OLIVE"
-	BANKSALAD Company = "BANKSALAD"
-	TOSS      Company = "TOSS"
-	DAANGN    Company = "DAANGN"
-	MUSINSA   Company = "MUSINSA"
-	TWONINE   Company = "29CM"
-	BUZZVIL   Company = "BUZZVIL"
-	KURLY     Company = "KURLY"
+	KAKAOPAY   Company = "KAKAOPAY"
+	KAKAOBANK  Company = "KAKAOBANK"
+	OLIVE      Company = "OLIVE"
+	BANKSALAD  Company = "BANKSALAD"
+	TOSS       Company = "TOSS"
+	DAANGN     Company = "DAANGN"
+	MUSINSA    Company = "MUSINSA"
+	TWONINE    Company = "29CM"
+	BUZZVIL    Company = "BUZZVIL"
+	KURLY      Company = "KURLY"
+	DEVSISTERS Company = "DEVSISTERS"
+	OHOUSE     Company = "OHOUSE"
+	LINE       Company = "LINE"
 )
