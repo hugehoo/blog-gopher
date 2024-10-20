2a7f9c3fc255cd75ade34752829f0660bba77755
hoo <tbnsok40@gmail.com>
2024-10-20 14:58:48 +0900

    feat: main.go code 정리

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"blog-gopher/common/dto"
-	"blog-gopher/common/types"
 	"blog-gopher/repository"
 	"blog-gopher/service"
 	"context"
-	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
@@ -14,14 +12,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
-	"sync"
 	"time"
 )
 
 var ginLambda *ginadapter.GinLambda
 
+func main() {
+	log.Println("📌 Start Lambda")
+	lambda.Start(Handler)
+	//localHandler()
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Handling request: %s %s", request.HTTPMethod, request.Path)
+	return ginLambda.ProxyWithContext(ctx, request)
+}
+
+// lambda handler
 func init() {
 	log.Println("✅ Starting initialization...")
 	gin.SetMode(gin.ReleaseMode)
@@ -51,26 +58,23 @@ func init() {
 	r.Use(cors.New(config))
 
 	r.GET("/posts", func(c *gin.Context) {
-		posts := getPosts(c, s)
-		c.JSON(http.StatusOK, posts)
+		results := s.GetPosts(c)
+		c.JSON(http.StatusOK, results)
 	})
 
 	r.GET("/search", func(c *gin.Context) {
-		c.JSON(http.StatusOK, searchPosts(c, s))
+		value := c.Query("keyword")
+		result := s.SearchPosts(value)
+		c.JSON(http.StatusOK, result)
 	})
 
 	r.GET("/search/blog/:blogId", func(c *gin.Context) {
-		c.JSON(http.StatusOK, getBlogById(c, s))
+		result := s.SearchPostsById(c.Param("blogId"))
+		c.JSON(http.StatusOK, dto.ConvertToDTO(result))
 	})
 
 	ginLambda = ginadapter.New(r)
 	log.Println("✅ Initialization complete")
-}
-
-func main() {
-	log.Println("📌 Start Lambda")
-	lambda.Start(Handler)
-	//localHandler()
 }
 
 func localHandler() {
@@ -100,157 +104,39 @@ func localHandler() {
 	r.Use(cors.New(config))
 
 	r.GET("/posts", func(c *gin.Context) {
-		posts := getPosts(c, s)
+		posts := s.GetPosts(c)
 		c.JSON(http.StatusOK, posts)
 	})
 
 	r.GET("/search", func(c *gin.Context) {
-		c.JSON(http.StatusOK, searchPosts(c, s))
+		value := c.Query("keyword")
+		result := s.SearchPosts(value)
+		c.JSON(http.StatusOK, result)
+	})
+
+	r.GET("/auto-search", func(c *gin.Context) {
+		keyword := c.Query("keyword")
+		response := s.AutoSearchPosts(keyword)
+		c.JSON(http.StatusOK, response)
 	})
 
 	r.GET("/search/blog/:blogId", func(c *gin.Context) {
-		c.JSON(http.StatusOK, getBlogById(c, s))
+		result := s.SearchPostsById(c.Param("blogId"))
+		c.JSON(http.StatusOK, dto.ConvertToDTO(result))
 	})
 
 	r.POST("/", func(c *gin.Context) {
 		s.UpdateAllPosts()
 	})
 
+	r.POST("/latest", func(c *gin.Context) {
+		s.UpdateLatestPosts()
+	})
+
 	r.POST("/content", func(c *gin.Context) {
-		updatePostContent(c, s)
+		posts := s.GetPosts(c)
+		s.UpdatePostContent(posts)
 	})
 
 	r.Run()
-}
-
-func updatePostContent(c *gin.Context, s *service.Service) {
-	var corp []string
-	posts := s.GetPosts(corp, 0, 1)
-	maxConcurrent := 10
-	semaphore := make(chan struct{}, maxConcurrent)
-
-	var wg sync.WaitGroup
-
-	for _, post := range posts {
-		wg.Add(1)
-		semaphore <- struct{}{} // 세마포어를 획득
-
-		go func(post types.Post) {
-			defer wg.Done()
-			defer func() { <-semaphore }() // 작업이 끝나면 세마포어를 해제
-
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			defer cancel()
-
-			req, err := http.NewRequestWithContext(ctx, "GET", post.Url, nil)
-			if err != nil {
-				log.Printf("Error creating request for post %s: %v", post.ID, err)
-				return
-			}
-
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil {
-				log.Printf("Error fetching URL for post %s: %v \n %v", post.ID, post.Url, err)
-				return
-			}
-			defer resp.Body.Close()
-
-			doc, err := goquery.NewDocumentFromReader(resp.Body)
-			if err != nil {
-				log.Printf("Error parsing HTML for post %s: %v", post.ID, err)
-				return
-			}
-
-			doc.Find("script, style").Remove()
-			text := strings.TrimSpace(doc.Text())
-			s.UpdatePost(post.ID, text)
-		}(post)
-	}
-
-	wg.Wait() // 모든 고루틴이 완료될 때까지 대기
-	log.Println("All posts have been processed")
-}
-
-func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Handling request: %s %s", request.HTTPMethod, request.Path)
-	return ginLambda.ProxyWithContext(ctx, request)
-}
-
-func searchPosts(c *gin.Context, s *service.Service) []dto.PostDTO {
-	pageStr := c.Query("page")         // 쿼리 파라미터에서 페이지 번호를 가져옵니다.
-	pageSizeStr := c.Query("pageSize") // 쿼리 파라미터에서 페이지 크기를 가져옵니다.
-
-	// 기본 값 설정
-	page := 1
-	pageSize := 20
-
-	// 쿼리 파라미터에서 페이지 번호와 페이지 크기를 정수로 변환
-	if pageStr != "" {
-		if p, err := strconv.Atoi(pageStr); err == nil {
-			page = p
-		}
-	}
-	if pageSizeStr != "" {
-		if ps, err := strconv.Atoi(pageSizeStr); err == nil {
-			pageSize = ps
-		}
-	}
-	value := c.Query("keyword")
-	var queryResult []types.Post
-	queryResult = s.SearchPosts(value, page, pageSize)
-
-	var result = make([]dto.PostDTO, len(queryResult))
-	for idx, post := range queryResult {
-		result[idx] = dto.ConvertToDTO(post)
-	}
-	return result
-}
-
-type Category struct {
-	Category string `form:"category"`
-}
-
-func getPosts(c *gin.Context, s *service.Service) []dto.PostDTO {
-	pageStr := c.Query("page")         // 쿼리 파라미터에서 페이지 번호를 가져옵니다.
-	pageSizeStr := c.Query("pageSize") // 쿼리 파라미터에서 페이지 크기를 가져옵니다.
-
-	// 기본 값 설정
-	page := 1
-	pageSize := 20
-
-	// 쿼리 파라미터에서 페이지 번호와 페이지 크기를 정수로 변환
-	if pageStr != "" {
-		if p, err := strconv.Atoi(pageStr); err == nil {
-			page = p
-		}
-	}
-	if pageSizeStr != "" {
-		if ps, err := strconv.Atoi(pageSizeStr); err == nil {
-			pageSize = ps
-		}
-	}
-
-	var category Category
-	if err := c.ShouldBind(&category); err != nil {
-		log.Fatalln("Something wrong", err)
-	}
-	categoryStrings := category.Category
-	var splits []string
-	if categoryStrings != "" {
-		splits = strings.Split(categoryStrings, ",")
-	} else {
-		splits = []string{}
-	}
-
-	results := s.GetPosts(splits, page, pageSize)
-	response := make([]dto.PostDTO, len(results))
-	for idx, result := range results {
-		response[idx] = dto.ConvertToDTO(result)
-	}
-	return response
-}
-
-func getBlogById(c *gin.Context, s *service.Service) dto.PostDTO {
-	result := s.SearchPostsById(c.Param("blogId"))
-	return dto.ConvertToDTO(result)
 }
