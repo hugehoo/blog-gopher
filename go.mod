43b2a7b1dc9a7e81ed0b0c1f6b2eb1f4851b919f
hoo <tbnsok40@gmail.com>
2024-12-03 01:43:16 +0900

    fix: main.go

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module blog-gopher
 
-go 1.21.0
+go 1.23
 
-toolchain go1.21.1
+toolchain go1.23.2
 
 require (
 	github.com/PuerkitoBio/goquery v1.9.2
@@ -10,14 +10,20 @@ require (
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
+	github.com/gocolly/colly v1.2.0
 	go.mongodb.org/mongo-driver v1.17.0
-	golang.org/x/net v0.29.0
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.3 // indirect
+	github.com/antchfx/xmlquery v1.4.2 // indirect
+	github.com/antchfx/xpath v1.3.2 // indirect
 	github.com/bytedance/sonic v1.12.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
+	github.com/chromedp/cdproto v0.0.0-20241110205750-a72e6703cd9b // indirect
+	github.com/chromedp/chromedp v0.11.2 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
@@ -25,18 +31,30 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -45,9 +63,11 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/arch v0.10.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"blog-gopher/repository"
 	"blog-gopher/service"
 	"context"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
@@ -24,7 +25,6 @@ func main() {
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Handling request: %s %s", request.HTTPMethod, request.Path)
 	return ginLambda.ProxyWithContext(ctx, request)
 }
 
@@ -34,13 +34,8 @@ func init() {
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	getPing(r)
 
-	//
 	var s *service.Service
 	if repository, err := repository.NewRepository(); err != nil {
 		panic(err)
@@ -48,25 +43,13 @@ func init() {
 		s = service.NewService(repository)
 	}
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"*"} // Allow all origins
-	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
-	config.ExposeHeaders = []string{"Content-Length"}
-	config.AllowCredentials = true
-	config.MaxAge = 12 * time.Hour
-	r.Use(cors.New(config))
+	cacheService := service.NewCacheService()
 
-	r.GET("/posts", func(c *gin.Context) {
-		results := s.GetPosts(c)
-		c.JSON(http.StatusOK, results)
-	})
+	webConfigurations(r)
 
-	r.GET("/search", func(c *gin.Context) {
-		value := c.Query("keyword")
-		result := s.SearchPosts(value)
-		c.JSON(http.StatusOK, result)
-	})
+	getAllPosts(r, cacheService, s)
+	getPostsByCorp(r, cacheService, s)
+	searchPosts(r, s)
 
 	r.GET("/search/blog/:blogId", func(c *gin.Context) {
 		result := s.SearchPostsById(c.Param("blogId"))
@@ -80,39 +63,21 @@ func init() {
 func localHandler() {
 	log.Println("📌 Start Local")
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	getPing(r)
 
-	//
 	var s *service.Service
 	if repository, err := repository.NewRepository(); err != nil {
 		panic(err)
 	} else {
 		s = service.NewService(repository)
 	}
+	cacheService := service.NewCacheService()
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"*"} // Allow all origins
-	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
-	config.ExposeHeaders = []string{"Content-Length"}
-	config.AllowCredentials = true
-	config.MaxAge = 12 * time.Hour
-	r.Use(cors.New(config))
+	webConfigurations(r)
 
-	r.GET("/posts", func(c *gin.Context) {
-		posts := s.GetPosts(c)
-		c.JSON(http.StatusOK, posts)
-	})
-
-	r.GET("/search", func(c *gin.Context) {
-		value := c.Query("keyword")
-		result := s.SearchPosts(value)
-		c.JSON(http.StatusOK, result)
-	})
+	getAllPosts(r, cacheService, s)
+	getPostsByCorp(r, cacheService, s)
+	searchPosts(r, s)
 
 	r.GET("/auto-search", func(c *gin.Context) {
 		keyword := c.Query("keyword")
@@ -138,5 +103,69 @@ func localHandler() {
 		s.UpdatePostContent(posts)
 	})
 
+	r.POST("/update-date", func(c *gin.Context) {
+		s.UpdateDate()
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
 	r.Run()
+}
+
+func webConfigurations(r *gin.Engine) {
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"} // Allow all origins
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+	r.Use(cors.New(config))
+}
+
+func getPing(r *gin.Engine) gin.IRoutes {
+	return r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+}
+
+func searchPosts(r *gin.Engine, s *service.Service) gin.IRoutes {
+	return r.GET("/search", func(c *gin.Context) {
+		value := c.Query("keyword")
+		result := s.SearchPosts(value)
+		c.JSON(http.StatusOK, result)
+	})
+}
+
+func getPostsByCorp(r *gin.Engine, cacheService *service.CacheService, s *service.Service) gin.IRoutes {
+	return r.GET("/corps", func(c *gin.Context) {
+		value := c.Query("corp")
+		key := fmt.Sprintf("corps::%s", value)
+		cache, exists := cacheService.GetPostsByCorp(key)
+		if exists {
+			log.Println("Hit the cache")
+			c.JSON(http.StatusOK, cache)
+		} else {
+			posts := s.GetPostsByCorp(value)
+			cacheService.SetPosts(key, posts)
+			c.JSON(http.StatusOK, posts)
+		}
+	})
+}
+
+func getAllPosts(r *gin.Engine, cacheService *service.CacheService, s *service.Service) gin.IRoutes {
+	return r.GET("/posts", func(c *gin.Context) {
+		key := "posts"
+		cache, bool := cacheService.GetPosts(key)
+		if bool != false {
+			c.JSON(http.StatusOK, cache)
+		} else {
+			results := s.GetPosts(c)
+			cacheService.SetPosts(key, results)
+			c.JSON(http.StatusOK, results)
+		}
+	})
 }
