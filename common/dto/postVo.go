6ed8c12d0dcba950267ce8401c5c6c90ff82bf4f
hoo <tbnsok40@gmail.com>
2025-01-24 16:04:53 +0900

    fix: adapt blog title html escaping

--- common/dto/postVo.go
+++ common/dto/postVo.go
@@ -4,11 +4,12 @@ import (
 	"blog-gopher/common/types"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"html"
 )
 
 func InsertPost(post types.Post) primitive.D {
 	return bson.D{
-		{"title", post.Title},
+		{"title", html.UnescapeString(post.Title)},
 		{"url", post.Url},
 		{"summary", post.Summary},
 		{"date", post.Date},
