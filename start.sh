6cc775f6064a5368e04a285965dc9c8b9595baf4
hoo <tbnsok40@gmail.com>
2025-02-18 00:49:23 +0900

    feat: auto start and terminate

---
+++ start.sh
@@ -0,0 +1,18 @@
+go run . &
+sleep 3 && command
+
+curl -X POST "http://localhost:8080/latest"
+
+sleep 4 && command
+
+# PID 가져오기
+PID=$(lsof -ti tcp:8080)
+
+# 가져온 PID로 프로세스 종료하기
+if [ ! -z "$PID" ]; then
+    kill -9 $PID
+    echo "Process $PID killed"
+else
+    echo "No process found on port 8080"
+fi
+echo "애플리케이션이 종료되었습니다."
